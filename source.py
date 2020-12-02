30406806d3ef59fbe766dfbcdc0682e01f04a588
aseemcr07 <60442085+aseemcr07@users.noreply.github.com>
2020-12-02 19:53:16 +0530

    Update source.py

--- source.py
+++ source.py
@@ -4,7 +4,6 @@ import tkinter as tk
 from tkinter import ttk
 from PIL import Image
 
-
 root = Tk()
 root.title("BookMyTicket")
 root.geometry('600x400')
@@ -26,29 +25,27 @@ tab4 = ttk.Frame(tab_control)
 # Add tabs to Notebook
 tab_control.add(tab1, text=f'{"Home":^30s}')
 tab_control.add(tab2, text=f'{"Bookings":^30s}')
-tab_control.add(tab3, text=f'{"Seating":^30s}', command=lambda: displaySeating())
+tab_control.add(tab3, text=f'{"Seating":^30s}')
 tab_control.add(tab4, text=f'{"About":^30s}')
 tab_control.pack(expand=1, fill="both")
 
 # HOME
-Label1 = Label(tab1, text='Welcome to JAG Theater',
+Label1 = Label(tab1, text='Welcome to GJA Theater',
                bg='#ffcc00', font=("Verdana", 24, "bold"))
 Label1.grid(row=1, column=2, columnspan=3, pady=70, padx=10)
 Label2 = Label(tab1, text='Book your ticket and enjoy the movie', bg='#ffcc00', font=("Verdana", 16, "italic")).grid(
     row=2, column=2, columnspan=3, padx=5)
 
-# Booking Section
 
+# Booking Section
 
 # Booking Success
 def bookingsuccess():
+    # Booking.destroy()
+    # showtime.destroy()
+    # root.destroy()
     tk.messagebox.showinfo(
         title="Success", message="Your Ticket has been booked successfully \n\n Thank you")
-
-def displaySeating():
-    # img = Image.open("Image location")
-    # img.show()
-    pass
 
 # Final Menu
 def menu2():
@@ -61,37 +58,56 @@ def menu2():
     typeL.grid(row=0, column=0, padx=10)
 
     tickettype = ttk.Combobox(Booking, state='readonly', width=50)
-    tickettype['values'] = ("gold")
+    tickettype['values'] = ("Silver(Rs. 100)", "Gold(Rs. 150)", "Platinum(Rs. 200)")
+
     tickettype.grid(row=0, column=1, columnspan=4, padx=10, pady=10)
 
-    ticketcountL = Label(Booking, text='No of tickets',  bg='#2fbdc6')
+    ticketcountL = Label(Booking, text='No of tickets', bg='#2fbdc6')
     ticketcountL.grid(row=1, column=0, padx=10)
 
     ticketcount = Entry(Booking, width=50)
     ticketcount.grid(row=1, column=1, columnspan=4, padx=10, pady=10)
 
-    calculate = Button(Booking, text="Calculate")
+    calculate = Button(Booking, text="Calculate", command=lambda: getAmount())
     calculate.grid(row=2, column=2)
 
-    totalL = Label(Booking, text='Total =',  bg='#2fbdc6')
+    ticketprices = [100, 150, 200]
+
+    totalL = Label(Booking, text='Total =', bg='#2fbdc6')
     totalL.grid(row=3, column=1, padx=10)
 
-    ticketcount = Entry(Booking, width=10, state='readonly')
-    ticketcount.grid(row=3, column=2, padx=10, pady=10)
+    amountdisplay = Entry(Booking, width=10)
+    amountdisplay.grid(row=3, column=2, padx=10, pady=10)
 
-    nameL = Label(Booking, text='Name',  bg='#2fbdc6')
+    def getAmount():
+        numtickets = ticketcount.get()
+        selectedtype = tickettype.get()
+        j = 0
+        for t in tickettype['values']:
+            if t == selectedtype:
+                break
+            else:
+                j += 1
+
+        selectedPrice = ticketprices[j]
+
+        amount = selectedPrice * int(numtickets)
+        print(amount)
+        amountdisplay.insert(END, str(amount))
+
+    nameL = Label(Booking, text='Name', bg='#2fbdc6')
     nameL.grid(row=4, column=0, padx=10)
 
     namecustomer = Entry(Booking, width=50)
     namecustomer.grid(row=4, column=1, columnspan=4, padx=10, pady=10)
 
-    phoneL = Label(Booking, text='Phone No',  bg='#2fbdc6')
+    phoneL = Label(Booking, text='Phone No', bg='#2fbdc6')
     phoneL.grid(row=5, column=0, padx=10)
 
     phonecustomer = Entry(Booking, width=50)
     phonecustomer.grid(row=5, column=1, columnspan=4, padx=10, pady=10)
 
-    PayemntmodeL = Label(Booking, text='Mode of Payment',  bg='#2fbdc6')
+    PayemntmodeL = Label(Booking, text='Mode of Payment', bg='#2fbdc6')
     PayemntmodeL.grid(row=6, column=0, padx=10)
 
     cash = tk.Checkbutton(Booking, text='Cash').grid(row=6, column=1)
@@ -101,6 +117,9 @@ def menu2():
                     bg='#76c6ba', command=bookingsuccess)
 
     paynow.grid(row=7, column=1, columnspan=2, pady=20, padx=10)
+
+    displayamount = Text(tab2, height=6, width=40)
+    displayamount.grid(row=2, column=1, columnspan=3, pady=30)
 
     cancel = Button(Booking, text="Cancel", bg='#e31a01',
                     fg='#fff', width=10, command=Booking.destroy)
@@ -181,7 +200,6 @@ moviedropdown.grid(row=0, column=1, columnspan=3, padx=20, pady=20)
 button1 = Button(tab2, text='Select', bg='#76c6ba', command=lambda: getMovie())
 button1.grid(row=1, column=2)
 
-
 display1 = Text(tab2, height=6, width=40)
 display1.grid(row=2, column=1, columnspan=3, pady=30)
 directors = ["Ramesh Sippy", "Aditya Chopra",
@@ -204,12 +222,13 @@ buttonexit = Button(tab2, text="Exit", bg='#e31a01',
                     fg='#fff', width=10, command=root.destroy)
 buttonexit.grid(row=4, column=3)
 
-
 # About
 about_label = Label(
-    tab4, text="BookMyTicket \n Movie Ticket Booking System \n\n Made by \n Gaurav Damodhar Kulkarni \n Jui Kiran Kulkarni \n Abdul Aseem Shaikh \n(As DSL Mini Project)", anchor="center",  font=("Times", 20))
+    tab4,
+    text="BookMyTicket \n Movie Ticket Booking System \n\n Made by \n Gaurav Kulkarni \n Jui Kulkarni \n Abdul Aseem Shaikh \n(As DSL Mini Project)",
+    anchor="center", font=("Times", 20))
 about_label.place(x=250, y=160, anchor="center")
 
-
 root.mainloop()
+
 
